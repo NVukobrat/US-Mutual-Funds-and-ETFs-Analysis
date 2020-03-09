44cbadfc763d97c4abc6ea4dac61c6a899e87de4
Nikola Vukobrat <nikola.vukobrat@gmail.com>
2020-03-09 18:19:00 +0100

    Progress

--- source.py
+++ source.py
@@ -7,45 +7,19 @@ from matplotlib import pyplot as plt
 def main():
     # Load dataset
     df = pd.read_csv('dataset/ETFs.csv', index_col=False)
-    df['count'] = 1  # Count column
 
-    # Clean
-    df.replace('', 'NaN', inplace=True)
-    df.replace('nan', 'NaN', inplace=True)
-
-    # Populate
-    df.replace('NaN', np.nan, inplace=True)
-    df = df.fillna(df.mean)
-
-    # Count(Bar) plots
-    count = [
-        'legal_type',
-        'investment',
-        'size',
-        'currency',
-    ]
-    for c in count:
-        chart = sns.countplot(df[c])
-        chart.set_xticklabels(
-            chart.get_xticklabels(),
-            rotation=30,
-            horizontalalignment='right',
-        )
+    num_unique_col = df.nunique()
+    df = df[[col for col in df if 1 < num_unique_col[col] < 50]]
+    for col in df:
+        df_column = df[col]
+        if np.issubdtype(type(df_column.iloc[0]), np.number):
+            df_column.hist()
+        else:
+            df_column.value_counts().plot().bar(x=df_column, height=90)
+        plt.ylabel('counts')
+        plt.xticks(rotation=30)
+        plt.title(col)
         plt.show()
-
-    # # Line
-    # line = [
-    #     'net_assets',
-    #     'ytd_return',
-    #     'fund_yield',
-    # ]
-    # for l in line:
-    #     df[l] = pd.to_numeric(df[l])
-    #
-    # for x in range(len(line)):
-    #     for y in range(x + 1, len(line)):
-    #         sns.lineplot(x=df[line[y]], y=df[line[x]])
-    #         plt.show()
 
 
 if __name__ == '__main__':
