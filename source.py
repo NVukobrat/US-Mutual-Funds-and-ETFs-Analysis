615b81e2b4f7bc41047df503bdc4c9e1bcfb54b4
Nikola Vukobrat <nikola.vukobrat@gmail.com>
2020-03-13 21:08:52 +0100

    Progress

--- source.py
+++ source.py
@@ -16,34 +16,51 @@ def load_dataset():
     return df_etf, df_mf
 
 
-# Use for ML part of the project
-def clean(df):
-    df.replace('', 'NaN', inplace=True)
-    df.replace('nan', 'NaN', inplace=True)
-    df.replace('NaN', np.nan, inplace=True)
+def types(df):
+    for c, t in zip(df, df.dtypes):
+        if np.issubdtype(t, np.number):
+            print("Num", c, t)
+        else:
+            print("Str", c, t)
 
-    num_mean = df.select_dtypes(np.number)
-    num_mean.join(df['fund_treynor_ratio_3years'])  # As consequence of too many NaN values
-    num_mean.join(df['category_treynor_ratio_5years'])  # As consequence of too many NaN values
 
-    str_mean = df[df.columns.difference(num_mean.columns)]
-    str_mean = str_mean.drop('fund_treynor_ratio_3years', axis=1)  # As consequence of too many NaN values
-    str_mean = str_mean.drop('category_treynor_ratio_5years', axis=1)  # As consequence of too many NaN values
-    str_mean.replace(np.nan, 'NaN', inplace=True)
-    le = LabelEncoder()
-    for col in str_mean:
-        df[col] = le.fit_transform(str_mean[col])
+def describe(df):
+    print("DataFrame summary:")
+    print(df.info())
+    print()
 
-    mu = num_mean.quantile(0)
-    sigma = num_mean.std(axis=0)
-    for col in num_mean:
-        stack = num_mean[col]
-        null_stack = stack[pd.isnull(stack)]
-        ran = np.random.normal(mu[col], sigma[col], len(null_stack))
-        stack.loc[null_stack.index] = ran
-        df[col] = stack.values
+    print("Columns summary:")
+    for col in list(df):
+        print(col)
+        print(df[col].dropna().describe())
+        print(df[col].dropna().value_counts())
+        print()
 
-    return df
+
+def hist_bar_plot(df, rc_num=(2, 6), size=(16 * 3, 9 * 3), unique=True):
+    # Chose adequate columns for visualization
+    if unique:
+        num_unique_col = df.nunique()
+        df = df[[col for col in df if 1 < num_unique_col[col] < 50]]
+
+    # Create Hist and Bar plots
+    plt.figure(figsize=size)
+    for i, col in enumerate(df):
+        plt.subplot(rc_num[0], rc_num[1], i + 1)
+        df_column = df[col]
+        if np.issubdtype(type(df_column.iloc[0]), np.number):
+            df_column.hist()
+        else:
+            df_column.value_counts().plot.bar()
+        plt.title(col, fontsize=25)
+        plt.xticks(rotation=30, fontsize=30)
+        plt.yticks(fontsize=30)
+        plt.ylabel('counts')
+
+        if i == 10:
+            break
+    plt.tight_layout(pad=1.0, w_pad=1.0, h_pad=1.0)
+    plt.show()
 
 
 def correlation_clean(df, threshold=(-0.9, -0.3, 0.3, 0.9)):
@@ -74,32 +91,6 @@ def correlation_clean(df, threshold=(-0.9, -0.3, 0.3, 0.9)):
     return df
 
 
-def hist_bar_plot(df, rc_num=(2, 6), size=(16 * 3, 9 * 3), unique=True):
-    # Chose adequate columns for visualization
-    if unique:
-        num_unique_col = df.nunique()
-        df = df[[col for col in df if 1 < num_unique_col[col] < 50]]
-
-    # Create Hist and Bar plots
-    plt.figure(figsize=size)
-    for i, col in enumerate(df):
-        plt.subplot(rc_num[0], rc_num[1], i + 1)
-        df_column = df[col]
-        if np.issubdtype(type(df_column.iloc[0]), np.number):
-            df_column.hist()
-        else:
-            df_column.value_counts().plot.bar()
-        plt.title(col, fontsize=25)
-        plt.xticks(rotation=30, fontsize=30)
-        plt.yticks(fontsize=30)
-        plt.ylabel('counts')
-
-        if i == 10:
-            break
-    plt.tight_layout(pad=1.0, w_pad=1.0, h_pad=1.0)
-    plt.show()
-
-
 def corr_mtx(df, fig_size=50, unique=True):
     sns.set(font_scale=4.0)
     # Chose adequate columns for visualization
@@ -126,32 +117,39 @@ def scatter_mtx(df, figsize=50, cap=10):
     plt.show()
 
 
-def types(df):
-    for c, t in zip(df, df.dtypes):
-        if np.issubdtype(t, np.number):
-            print("Num", c, t)
-        else:
-            print("Str", c, t)
+# TODO: Use for ML part of the project
+def clean(df):
+    df.replace('', 'NaN', inplace=True)
+    df.replace('nan', 'NaN', inplace=True)
+    df.replace('NaN', np.nan, inplace=True)
+
+    num_mean = df.select_dtypes(np.number)
+    num_mean.join(df['fund_treynor_ratio_3years'])  # As consequence of too many NaN values
+    num_mean.join(df['category_treynor_ratio_5years'])  # As consequence of too many NaN values
+
+    str_mean = df[df.columns.difference(num_mean.columns)]
+    str_mean = str_mean.drop('fund_treynor_ratio_3years', axis=1)  # As consequence of too many NaN values
+    str_mean = str_mean.drop('category_treynor_ratio_5years', axis=1)  # As consequence of too many NaN values
+    str_mean.replace(np.nan, 'NaN', inplace=True)
+    le = LabelEncoder()
+    for col in str_mean:
+        df[col] = le.fit_transform(str_mean[col])
+
+    mu = num_mean.quantile(0)
+    sigma = num_mean.std(axis=0)
+    for col in num_mean:
+        stack = num_mean[col]
+        null_stack = stack[pd.isnull(stack)]
+        ran = np.random.normal(mu[col], sigma[col], len(null_stack))
+        stack.loc[null_stack.index] = ran
+        df[col] = stack.values
+
+    return df
 
 
 def main():
     pre_config()
     df_etf, df_mf = load_dataset()
-
-    # ETFs analysis
-    hist_bar_plot(df_etf)
-    df_etf = correlation_clean(df_etf)
-    corr_mtx(df_etf, unique=False)
-    scatter_mtx(df_etf)
-
-    # MF analysis
-    hist_bar_plot(df_mf)
-    df_mf = correlation_clean(df_mf, threshold=(-0.9, -0.6, 0.6, 0.9))
-    corr_mtx(df_mf, unique=False)
-    scatter_mtx(df_mf, cap=20)
-
-    # ML
-    # df_etf = clean(df_etf)
 
 
 if __name__ == '__main__':
